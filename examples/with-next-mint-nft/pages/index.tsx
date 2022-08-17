2866dd366a896a644c60481ed41d83f4f61e1858
missphyrgames <missphyrgames@gmail.com>
2022-08-17 19:41:10 +0100

    Update version and remove Image component

--- examples/with-next-mint-nft/pages/index.tsx
+++ examples/with-next-mint-nft/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 import {
@@ -98,25 +97,13 @@ const Home: NextPage = () => {
         <div style={{ flex: '0 0 auto' }}>
           <FlipCard>
             <FrontCard isCardFlipped={isMinted}>
-              <Image
-                layout="responsive"
-                src="/nft.png"
-                width="500"
-                height="500"
-                alt="RainbowKit Demo NFT"
-              />
+              <img src="/nft.png" alt="RainbowKit Demo NFT" width="500" height="500">
               <h1 style={{ marginTop: 24 }}>Rainbow NFT</h1>
               <ConnectButton />
             </FrontCard>
             <BackCard isCardFlipped={isMinted}>
               <div style={{ padding: 24 }}>
-                <Image
-                  src="/nft.png"
-                  width="80"
-                  height="80"
-                  alt="RainbowKit Demo NFT"
-                  style={{ borderRadius: 8 }}
-                />
+                <img src="/nft.png" alt="RainbowKit Demo NFT" width="80" height="80">
                 <h2 style={{ marginTop: 24, marginBottom: 6 }}>NFT Minted!</h2>
                 <p style={{ marginBottom: 24 }}>
                   Your NFT will show up in your wallet in the next few minutes.
